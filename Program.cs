3e90ae964fab5a855583e0c66a94ba60959ac9b2
Mari <102271182+Marinaiebxl@users.noreply.github.com>
2024-02-12 11:19:52 -0400

    Terminado

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        
-        
+        string Ants = "..ant..ant.ant...ant.ant..ant.ant....ant..ant.ant.ant...ant..";
+        string AntStampede = " ...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t";
+
+        int AliveBefore = Ants.Count(c => c == '.');
+        int AliveAfter = AntStampede.Count(c => c == '.');
+
+        int Deadants = AliveBefore - AliveAfter;
+        Console.WriteLine(Deadants);
+        Console.ReadLine();
+
+
+
+
     }
 }
